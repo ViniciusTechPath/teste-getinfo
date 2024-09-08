04d08d55858e3643b47fb768ad530d9f98ea2ad2
vVinicisz <159086837+ViniBits@users.noreply.github.com>
2024-09-07 22:33:16 -0300

    main

---
+++ static/index.js
@@ -0,0 +1,24 @@
+// dropdown menu (enable/desable)
+
+document.addEventListener('DOMContentLoaded', function () {
+    const profileToggle = document.getElementById('profile-toggle');
+    const dropdownIcon = document.getElementById('dropdown-icon');
+    const dropdown = document.querySelector('.dropdown');
+
+    profileToggle.addEventListener('click', function () { // verificar
+        const isExpanded = profileToggle.getAttribute('aria-expanded') === 'true';
+
+        dropdownIcon.classList.toggle('bi-chevron-up', !isExpanded);
+        dropdownIcon.classList.toggle('bi-chevron-down', isExpanded);
+    });
+
+    dropdown.addEventListener('hidden.bs.dropdown', function () {
+        dropdownIcon.classList.remove('bi-chevron-up');
+        dropdownIcon.classList.add('bi-chevron-down');
+    });
+
+    dropdown.addEventListener('shown.bs.dropdown', function () {
+        dropdownIcon.classList.remove('bi-chevron-down');
+        dropdownIcon.classList.add('bi-chevron-up');
+    });
+});
