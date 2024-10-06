d38b627faa5b6372f265f563ab0f87eb209eb5e5
vVinicisz <159086837+ViniBits@users.noreply.github.com>
2024-10-06 20:29:27 -0300

    fixando-responsividade

--- static/index.js
+++ static/index.js
@@ -1,24 +1,7 @@
-// dropdown menu (enable/desable)
-
 document.addEventListener('DOMContentLoaded', function () {
-    const profileToggle = document.getElementById('profile-toggle');
-    const dropdownIcon = document.getElementById('dropdown-icon');
-    const dropdown = document.querySelector('.dropdown');
+    const dropdownToggle = document.getElementById('dropdownUser1');
 
-    profileToggle.addEventListener('click', function () { // verificar
-        const isExpanded = profileToggle.getAttribute('aria-expanded') === 'false';
-
-        dropdownIcon.classList.toggle('bi-chevron-up', !isExpanded);
-        dropdownIcon.classList.toggle('bi-chevron-down', isExpanded);
-    });
-
-    dropdown.addEventListener('hidden.bs.dropdown', function () {
-        dropdownIcon.classList.remove('bi-chevron-up');
-        dropdownIcon.classList.add('bi-chevron-down');
-    });
-
-    dropdown.addEventListener('shown.bs.dropdown', function () {
-        dropdownIcon.classList.remove('bi-chevron-down');
-        dropdownIcon.classList.add('bi-chevron-up');
+    dropdownToggle.addEventListener('click', function () {
+        dropdownToggle.classList.toggle('rotate');
     });
 });
